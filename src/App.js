8288427e43100f0c4d9ad871d17308be038de582
Anastasia Orlova <anastasia2804@gmail.com>
2022-12-07 15:07:56 -0500

    controlled components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [myInput, setMyInput] = useState("");
+
+  const [foodArray, setFoodArray] = useState([
+    "pizza",
+    "banana pancakes",
+    "apple pie",
+  ]);
+
+  const [filteredFoodArray, setFilteredFoodArray] = useState([
+    "pizza",
+    "banana pancakes",
+    "apple pie",
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <span>Search Bar</span>
+      <input onChange={(event) => {
+        const newList = foodArray.filter(singleFood => {
+          return singleFood.toLowerCase().includes(event.target.value.toLowerCase())
+        }) 
+        console.log(newList)
+        setFilteredFoodArray(newList)
+      }} />
+
+    </div>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          console.log("our state is", myInput);
+          const copyFoodArray = [...foodArray];
+          copyFoodArray.push(myInput);
+          setFilteredFoodArray(copyFoodArray);
+          setMyInput('')
+        }}
+      >
+        <label>type here</label>
+        <input
+          type="text"
+          value={myInput}
+          onChange={(event) => {
+            console.log("it works", event.target.value);
+            setMyInput(event.target.value);
+          }}
+        />
+        <button>Submit</button>
+      </form>
+      <div>
+      <ol>
+        {filteredFoodArray.map(singleFood => {
+          return (
+            <li>{singleFood}</li>
+          )
+        }) }
+      </ol>
+      </div>
     </div>
   );
 }
